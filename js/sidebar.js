20d985915a992579ba08395f8b4b5f7716226cfb
microwaveabletoaster <jrobdikeman@gmail.com>
2014-09-15 20:33:23 -0500

    made everything SO SEXY UGH

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,8 @@
 var element = document.getElementById('side');
 var midPanel = document.getElementById('center');
 var nodeList = document.getElementsByClassName('sideNav');
+var sideLinkList = document.getElementsByClassName('sideLink');
+
 
 var out = false;
 
@@ -15,6 +17,7 @@ midPanel.style.width = wFourth*2;
 for (var i = 1, length = nodeList.length; i < length; i+=2)
 {
 	nodeList[i].style.backgroundColor = '#33CCCC';
+	sideLinkList[i-1].style.color = '#75FFFF';
 } 
 
 
