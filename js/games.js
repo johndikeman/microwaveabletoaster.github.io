b5d363ff699ea923a136183ef70b7fab4f0d9b18
microwaveabletoaster <jrobdikeman@gmail.com>
2014-09-18 18:17:26 -0500

    fixed some links not w0rking

---
+++ js/games.js
@@ -0,0 +1,10 @@
+var center = document.getElementById('center');
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+center.style.width = width-200;
+center.style.height = height-50;
+
+thumbHolderWidth = (width-200)/5;
+thumbHolderHeight = (height-50)/3;
