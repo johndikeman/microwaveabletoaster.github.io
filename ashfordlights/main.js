2c25cd23700a7a6420fb1ab85ec9008668f3b03a
John <jrobdikeman@gmail.com>
2015-02-17 17:18:45 -0600

    made a simple visualizer that displays the average frequency of the song with a highpass filter applied to it. swag.

--- ashfordlights/main.js
+++ ashfordlights/main.js
@@ -1,41 +1,55 @@
 window.onload = function() {
-  var ctx = new (window.AudioContext||window.webkitAudioContext)();
-  var audio = document.querySelector('audio');
-  var output = document.getElementById("output");
-  var audioSrc = ctx.createMediaElementSource(audio);
-  var analyser = ctx.createAnalyser();
-  // we have to connect the MediaElementSource with the analyser 
-  audioSrc.connect(analyser);
-  // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
- 
-  // frequencyBinCount tells you how many values you'll receive from the analyser
-  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
- 
-  // we're ready to receive some data!
-  // loop
+    var ctx = new (window.AudioContext||window.webkitAudioContext)();
+    var audio = document.querySelector('audio');
+    var output = document.getElementById("output");
+    
+    var audioSrc = ctx.createMediaElementSource(audio);
+    var analyser = ctx.createAnalyser();
+    var filter = ctx.createBiquadFilter();
+    filter.type = 'highshelf';
+    filter.frequency.value = 7000;
+    filter.gain.value = 50;
 
-  function init(){
-    for(var a=0;a<frequencyData.length;a++){ 
-      console.log(a);
-      var div = document.createElement('span');
-      div.innerHTML = a;
-      document.getElementsByTagName('body')[0].appendChild(div);
+    // we have to connect the MediaElementSource with the analyser 
+    audioSrc.connect(filter);
+    filter.connect(analyser);
+    audioSrc.connect(ctx.destination);
+    // analyser.connect(ctx.destination);
+
+    // frequencyBinCount tells you how many values you'll receive from the analyser
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+    // we're ready to receive some data!
+    // loop
+
+    var div = document.getElementById("pulse");
+
+
+    function renderFrame() {
+        total = count = 0;
+        requestAnimationFrame(renderFrame);
+
+        // update data in frequencyData
+        analyser.getByteFrequencyData(frequencyData);
+        // render frame based on values in frequencyData
+        //output.innerHTML = Array.apply([], frequencyData).join(",");
+
+        for(var a=0;a<frequencyData.length;a++){
+            if(frequencyData[a]!=0)
+                count+=1; total += frequencyData[a]
+        }
+
+        avrg = total/count;
+        div.style.width = div.style.height = Math.floor(avrg);
+        div.style.borderRadius = ''+Math.floor(Math.floor(avrg)/2)+'px';
+        div.style.marginLeft = div.style.marginTop = -1*(Math.floor(avrg)/2)
+
+
     }
-  }
 
-  function renderFrame() {
-     requestAnimationFrame(renderFrame);
-
-     // update data in frequencyData
-     analyser.getByteFrequencyData(frequencyData);
-     // render frame based on values in frequencyData
-     //output.innerHTML = Array.apply([], frequencyData).join(",");
-  }
-  analyser.connect(ctx.destination);
-  init();
-  renderFrame();
+    renderFrame();
 
 
-  
+
 };
 
