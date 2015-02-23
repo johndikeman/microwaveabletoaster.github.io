3a8ee29d645524e38f77ac77503a159845d4bb3a
John <jrobdikeman@gmail.com>
2015-02-23 15:48:23 -0600

    Added twitter icons and shiz

---
+++ js/controllers.js
@@ -0,0 +1,8 @@
+var lbsApp = angular.module('lbsApp', []);
+
+lbsApp.controller('games', function ($scope) {
+  $scope.games = [
+    {'name': 'Game1',
+     'description': 'this is a game that i totally built'},
+  ];
+});
