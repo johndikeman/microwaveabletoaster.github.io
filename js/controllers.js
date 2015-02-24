cac7b09971c37df4070b5a11fdf21e190e27c817
John <jrobdikeman@gmail.com>
2015-02-24 15:47:34 -0600

    ang
    trying to implement more angularJS into the layout

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,42 @@
-var lbsApp = angular.module('lbsApp', []);
+// script.js
 
-lbsApp.controller('games', function ($scope) {
-  $scope.games = [
-    {'name': 'Game1',
-     'description': 'this is a game that i totally built',
-     'link':'https://www.google.com'
- 	},
+    // create the module and name it scotchApp
+        // also include ngRoute for all our routing needs
+    var lbsApp = angular.module('lbsApp', ['ngRoute']);
 
-  ];
-});
+    // configure our routes
+    lbsApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'index.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/games', {
+                templateUrl : 'games.html',
+                controller  : 'gamesController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'software.html',
+                controller  : 'softwareController'
+            });
+    });
+
+    // create the controller and inject Angular's $scope
+    lbsApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+
+    lbsApp.controller('gamesController', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
+
+    lbsApp.controller('softwareController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
