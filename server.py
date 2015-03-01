244f229dfc6d02dc3dee59301eb5c56c5641cb61
John <jrobdikeman@gmail.com>
2015-03-01 12:10:42 -0600

    angular bug

--- server.py
+++ server.py
@@ -1,29 +1,21 @@
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import sys
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-PORT_NUMBER = 8080
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
-	
-	#Handler for the GET requests
-	# def do_GET(self):
-	# 	self.send_response(200)
-	# 	self.send_header('Content-type','text/html')
-	# 	self.end_headers()
-	# 	# Send the html message
-	# 	self.wfile.write("Hello World !")
-	# 	return
+HandlerClass = SimpleHTTPRequestHandler
+ServerClass  = BaseHTTPServer.HTTPServer
+Protocol     = "HTTP/1.0"
 
-try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	
-	#Wait forever for incoming htto requests
-	server.serve_forever()
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8000
+server_address = ('127.0.0.1', port)
 
-except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
-	server.socket.close()
+HandlerClass.protocol_version = Protocol
+httpd = ServerClass(server_address, HandlerClass)
+
+sa = httpd.socket.getsockname()
+print "Serving HTTP on", sa[0], "port", sa[1], "..."
+httpd.serve_forever()
