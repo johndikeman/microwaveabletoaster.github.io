12201886e4c05fe14d6392e47856a27fb41380ac
John <jrobdikeman@gmail.com>
2015-03-01 12:53:49 -0600

    removed the annoying theatre.js title

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
-var theater = new TheaterJS();
-theater
-	.on('say:end',function(){
-		$('#mainBody')
-			.css('visibility','visible')
-			.addClass('animated fadeIn');
-		})
 
-	.describe("john", { speed: .7, accuracy: .8, invincibility: 4 }, "#title")
-	.write("john:lil brother softworks.", 750)
 
